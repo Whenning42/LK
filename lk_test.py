0b06afdffcbc563532fb88acaf89fa302e325c10
William Henning <wchenningwchenning@gmail.com>
2020-12-06 11:32:08 -0500

    Test fix-up

--- lk_test.py
+++ lk_test.py
@@ -9,14 +9,14 @@ def TestGaussianPyramid():
     assert(len(pyramid) == 3)
     assert(np.array_equal(orig, pyramid[0]))
 
-    # We don't put overly tight constraints on the downsampled pyramid values to allow us to change
+    # We don't put overly tight constraints on the expected pyramid values to allow us to change
     # filter sizes without breaking this test.
     assert(pyramid[1].shape == (32, 32))
-    assert(pyramid[1][2, 2] > .8)
+    assert(pyramid[1][2, 2] == 1)
     assert(pyramid[1][20, 20] == 0)
 
     assert(pyramid[2].shape == (16, 16))
-    assert(pyramid[2][1, 1] > .8)
+    assert(pyramid[2][1, 1] == 1)
     assert(pyramid[2][10, 10] == 0)
 
 TestGaussianPyramid()
